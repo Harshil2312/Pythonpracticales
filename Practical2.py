4efc1911f483b11391b7dd3eaf9da6e6e8104c3a
Harshil <88246447+Harshil2312@users.noreply.github.com>
2022-01-14 20:50:46 +0530

    Update Practical2.py

--- Practical2.py
+++ Practical2.py
@@ -10,7 +10,7 @@ my_dict1 = {'firstName': 'Harshil',
             'lastName': 'Padasala'}
 
 print(f"my_dict1: {my_dict1}")
-print(f"\n'firstName' in my_dict1: {'firstName' in my_dict1}")
+print(f"\n'firstName' in my_dict1: {'firstName' in my_dict1}") # ---> Checks 'firstName' is in dict1 or not
 # --> or print(f"\n 'firstName' in my_dict1: {'firstName' in my_dict1.keys()}")
 print(f"'middleName' in my_dict1: {'middleName' in my_dict1}")
 
@@ -21,7 +21,7 @@ my_dict2 = {'FirstName': 'Krishna',
 
 # --> update() function update the my_dict1 dictionary
 my_dict1.update(my_dict2)
-print(f'\n\nmerge dictionary: {my_dict1}')
+print(f'\n\nmerge dictionary: {my_dict1}') # ---> update dict1 with combining dict2
 
 # --> c. Write a Python program to sum all the items in a dictionary.
 
@@ -39,7 +39,7 @@ print(f"Sum of keys is {sum(my_dict)}")  # --> print the sum of keys
 
 sample = {0: 10, 1: 20}
 print(f"\nsample = {sample}")
-sample.update({2: 30})
+sample.update({2: 30})  # ---> update the dictionary with adding element (2: 30)
 print(f"after adding sample = {sample}")
 
 # --> e. Write a Python script to concatenate following dictionaries to create a new one.
@@ -55,9 +55,13 @@ dic3 = {5: 50, 6: 60}
 extended = {}
 print(f"\nextended = {extended}")
 extended.update(dic1)  # --> update dictionary extended after adding dic1
-extended.update(dic2)
-extended.update(dic3)
+extended.update(dic2)  # --> update dictionary extended after adding dic2
+extended.update(dic3)  # --> update dictionary extended after adding dic3
 print(f"After adding other dictionaries extended = {extended}")
+
+
+
+
 
 # ********************************************** Tuple Practicals **********************************************
 
@@ -82,13 +86,13 @@ print(f"After adding list in tuple tup = {tup}")
 # --> d. Write a Python program to convert a tuple to a string.
 
 tup = ('H', 'a', 'r', 's', 'h', 'i', 'l')
-string = "".join(tup)
+string = "".join(tup) # --> joining the tup elements with string ""
 print(f"\nTuple tup = {tup}\n string = {string}")
 
 # --> e. Write a Python program to find the length of a tuple.
 
 tup = ('Harshil', 64, ['list', 'krishna'], {1: 'dictionary'}, {'Set', '20CE064'})
-print(f"tup = {tup}\nLength of tup = {len(tup)}")
+print(f"tup = {tup}\nLength of tup = {len(tup)}")    # --> usig len function we can find length of any python data-structure
 
 # ********************************************** Set **********************************************
 
@@ -96,38 +100,38 @@ print("\n\n\n********************************************** Set ****************
 # --> a. Write a Python program to add member(s) in a set and clear a set.
 set1 = {'Harshil', (12, 4)}
 print(f"\n\nset1 = {set1}")
-set1.add('harshil')
+set1.add('harshil') # ---> add 'harshil' in set1
 print(f"After adding set1 = {set1}")
-set1.clear()
+set1.clear() # ---> clear the set1 or make it empty
 print(f"Clear the set\nset1 = {set1}")
 
 # --> b. Write a Python program to remove an item from a set if it is present in the set.
 set1 = {'Harshil'}
 print(f"\n'Harshil' in set1 = {'Harshil' in set1}")
-if 'Harshil' in set1:
-    set1.remove('Harshil')
+if 'Harshil' in set1: # --> check the particular element is in set1 or not
+    set1.remove('Harshil')  # ---> if element in set1 then remove it
 print(set1)
 
 # --> c. Write a Python program to create an intersection, Union, difference of sets.
 set1 = {1, 2, 3, 4, 5, 6, 7}
 set2 = {5, 6, 7, 8, 9, 10}
 
-intersection_ = set1.intersection(set2)
-union_ = set1.union(set2)
-difference_ = set1.difference(set2)
+intersection_ = set1.intersection(set2)  # --> create intersection
+union_ = set1.union(set2) # --> create union
+difference_ = set1.difference(set2) # --> create difference
 
 print(f"\nset1 = {set1}\nset2 = {set2}\nintersection = {intersection_}\nunion = {union_}\ndifference = {difference_}")
 
 # --> d. Write a Python program to find maximum and the minimum value in a set.
 set1 = {4, 9, 8, 12}
-temp_list = list(set1)
-temp_list.sort()
-max_number = temp_list[-1]
-min_number = temp_list[0]
+temp_list = list(set1) # ---> convert the set into list
+temp_list.sort() # ---> sorting the list
+max_number = temp_list[-1] # ---> afer sorting last element is maximum  
+min_number = temp_list[0] # ---> after sorting first element is minimum 
 print(f"max number = {max_number} \t\t\t min number = {min_number}")
 
 # --> e. Write a Python program to find the most common elements and their counts from list, tuple, dictionary.
 list1 = [1, 2, 3, 4, 1, 2, 3, 6, 8, 9, 1, 2, 7, 9, 9, 92, 1, 2, ]
-set1 = set(list1)
+set1 = set(list1) # ---> convert list to set for removinf duplicates
 for number in set1:
-    print(f"number = {number}, \tcount in list = {list1.count(number)}")
+    print(f"number = {number}, \tcount in list = {list1.count(number)}") # ---> iterate over a set and invoking count function to count the most comman element in list
